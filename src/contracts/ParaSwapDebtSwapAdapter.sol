0b847c76d1c133ba7e4b306a4aef70c1e9760cd3
Lukas <lukasstrassel@googlemail.com>
2023-03-20 22:58:13 +0100

    feat: credit delegation
    fix: remove obsolete contract

--- src/contracts/ParaSwapDebtSwapAdapter.sol
+++ src/contracts/ParaSwapDebtSwapAdapter.sol
@@ -25,124 +25,148 @@ contract ParaSwapDebtSwapAdapter is
 {
   using SafeMath for uint256;
 
+  uint16 constant REFERRER = 100;
+
+  mapping(address => IERC20WithPermit) public aTokens;
+  mapping(address => IERC20WithPermit) public vTokens;
+  mapping(address => IERC20WithPermit) public sTokens;
+
   constructor(
     IPoolAddressesProvider addressesProvider,
     IParaSwapAugustusRegistry augustusRegistry,
     address owner
   ) BaseParaSwapBuyAdapter(addressesProvider, augustusRegistry) {
     transferOwnership(owner);
+    cacheReserves();
+  }
+
+  /**
+   * @dev caches all reserves
+   */
+  function cacheReserves() public {
+    address[] memory reserves = POOL.getReservesList();
+    for (uint256 i = 0; i < reserves.length; i++) {
+      if (address(aTokens[reserves[i]]) == address(0)) {
+        cacheReserve(reserves[i]);
+      }
+    }
+  }
+
+  /**
+   * @dev Adds a reserve to the cache & renewes the approval
+   */
+  function cacheReserve(address reserve) public {
+    DataTypes.ReserveData memory reserveData = _getReserveData(reserve);
+    aTokens[reserve] = IERC20WithPermit(reserveData.aTokenAddress);
+    vTokens[reserve] = IERC20WithPermit(reserveData.variableDebtTokenAddress);
+    sTokens[reserve] = IERC20WithPermit(reserveData.stableDebtTokenAddress);
+
+    IERC20WithPermit(reserve).approve(address(POOL), 0);
+    IERC20WithPermit(reserve).approve(address(POOL), type(uint256).max);
+  }
+
+  /**
+   * @notice Swaps one debt(variable or stable) to another debt(variable)
+   * @dev Performs the following actions:
+   * 0. delegate credit in new debt asset to the debtSwap
+   * 1. flash current debt asset
+   * 2. repay specified amount of debt
+   * 3. borrow new debt + potential swap slippage
+   * 4. swap borrowed asset exact out to current debt
+   * 5. repay flashloan
+   * 6. repay new debt with leftover
+   * @param debtAsset The asset of current debt
+   * @param rateMode The rateMode of the debtAsset
+   * @param debtAmount The amount of debt to be swapped
+   * @param newDebtAsset The asset in which the new variable debt is created
+   */
+  function swapDebt(
+    address debtAsset,
+    uint256 debtAmount,
+    address newDebtAsset,
+    uint256 buyAllBalanceOffset,
+    uint256 rateMode,
+    bytes memory paraswapData
+  ) public {
+    // TODO: credit delegation
+    POOL.flashLoanSimple(
+      address(this),
+      debtAsset,
+      debtAmount + 1 ether, // TODO: should be exact premium
+      abi.encode(
+        newDebtAsset,
+        buyAllBalanceOffset,
+        rateMode,
+        paraswapData,
+        msg.sender
+      ),
+      REFERRER
+    );
   }
 
   /**
    * @notice Executes an operation after receiving the flash-borrowed assets
    * @dev Ensure that the contract can return the debt + premium, e.g., has
    *      enough funds to repay and has approved the Pool to pull the total amount
-   * @param assets The addresses of the flash-borrowed assets
-   * @param amounts The amounts of the flash-borrowed assets
-   * @param premiums The fee of each flash-borrowed asset
+   * @param asset The addresses of the flash-borrowed assets
+   * @param amount The amounts of the flash-borrowed assets
+   * @param premium The fee of each flash-borrowed asset
    * @param initiator The address of the flashloan initiator
    * @param params The byte-encoded params passed when initiating the flashloan
    * @return True if the execution of the operation succeeds, false otherwise
    */
   function executeOperation(
-    address[] calldata assets,
-    uint256[] calldata amounts,
-    uint256[] calldata premiums,
+    address asset,
+    uint256 amount,
+    uint256 premium,
     address initiator,
     bytes calldata params
   ) external returns (bool) {
     require(msg.sender == address(POOL), 'CALLER_MUST_BE_POOL');
+    require(initiator == address(this), 'INITIATIOR_MUST_BE_DEBTSWAP');
 
-    uint256 newDebtAmount = amounts[0];
-    address initiatorLocal = initiator;
+    (
+      IERC20Detailed newDebtAsset,
+      uint256 maxNewDebtAmount,
+      uint256 buyAllBalanceOffset,
+      uint256 rateMode,
+      bytes memory paraswapData,
+      address user
+    ) = abi.decode(
+        params,
+        (IERC20Detailed, uint256, uint256, uint256, bytes, address)
+      );
 
-    IERC20Detailed newDebtAsset = IERC20Detailed(assets[0]);
+    // 1. repay debt with flashed asset
+    POOL.repay(address(asset), amount, rateMode, user);
 
-    _swapAndRepay(
-      params,
-      premiums[0],
-      initiatorLocal,
+    // 2. borrow on behalf of the user
+    POOL.borrow(
+      address(newDebtAsset),
+      maxNewDebtAmount,
+      uint256(DataTypes.InterestRateMode.VARIABLE),
+      REFERRER,
+      user
+    );
+
+    // 3. swap newDebtAsset to flashedAsset with exact out
+    uint256 amountSold = _buyOnParaSwap(
+      0,
+      paraswapData,
       newDebtAsset,
-      newDebtAmount
+      IERC20Detailed(asset),
+      maxNewDebtAmount,
+      amount
+    );
+
+    // 4. repay newDebtAsset with leftovers
+    POOL.repay(
+      address(newDebtAsset),
+      newDebtAsset.balanceOf(address(this)),
+      uint256(DataTypes.InterestRateMode.VARIABLE),
+      user
     );
 
     return true;
-  }
-
-  /**
-   * @dev Swaps the flashed token to the debt token & repays the debt.
-   * @param premium Fee of the flash loan
-   * @param initiator Address of the user
-   * @param newDebtAsset Address of token to be swapped
-   * @param newDebtAmount Amount of the reserve to be swapped(flash loan amount)
-   */
-  function _swapAndRepay(
-    bytes calldata params,
-    uint256 premium,
-    address initiator,
-    IERC20Detailed newDebtAsset,
-    uint256 newDebtAmount
-  ) private {
-    (
-      IERC20Detailed debtAsset,
-      uint256 debtRepayAmount,
-      uint256 buyAllBalanceOffset,
-      uint256 rateMode,
-      bytes memory paraswapData
-    ) = abi.decode(params, (IERC20Detailed, uint256, uint256, uint256, bytes));
-
-    debtRepayAmount = getDebtRepayAmount(
-      debtAsset,
-      rateMode,
-      buyAllBalanceOffset,
-      debtRepayAmount,
-      initiator
-    );
-
-    uint256 amountSold = _buyOnParaSwap(
-      buyAllBalanceOffset,
-      paraswapData,
-      newDebtAsset,
-      debtAsset,
-      newDebtAmount,
-      debtRepayAmount
-    );
-
-    // Repay debt. Approves for 0 first to comply with tokens that implement the anti frontrunning approval fix.
-    IERC20(debtAsset).approve(address(POOL), 0);
-    IERC20(debtAsset).approve(address(POOL), debtRepayAmount);
-    POOL.repay(address(debtAsset), debtRepayAmount, rateMode, initiator);
-
-    // Repay flashloan with excess. Approves for 0 first to comply with tokens that implement the anti frontrunning approval fix.
-    IERC20(newDebtAsset).approve(address(POOL), 0);
-    IERC20(newDebtAsset).approve(address(POOL), newDebtAmount.add(premium));
-  }
-
-  function getDebtRepayAmount(
-    IERC20Detailed debtAsset,
-    uint256 rateMode,
-    uint256 buyAllBalanceOffset,
-    uint256 debtRepayAmount,
-    address initiator
-  ) private view returns (uint256) {
-    DataTypes.ReserveData memory debtReserveData = _getReserveData(
-      address(debtAsset)
-    );
-
-    address debtToken = DataTypes.InterestRateMode(rateMode) ==
-      DataTypes.InterestRateMode.STABLE
-      ? debtReserveData.stableDebtTokenAddress
-      : debtReserveData.variableDebtTokenAddress;
-
-    uint256 currentDebt = IERC20(debtToken).balanceOf(initiator);
-
-    if (buyAllBalanceOffset != 0) {
-      require(currentDebt <= debtRepayAmount, 'INSUFFICIENT_AMOUNT_TO_REPAY');
-      debtRepayAmount = currentDebt;
-    } else {
-      require(debtRepayAmount <= currentDebt, 'INVALID_DEBT_REPAY_AMOUNT');
-    }
-
-    return debtRepayAmount;
   }
 }
